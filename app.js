abe0ba356eb91a9c3bcbdd3ef5f526f9ade030f6
sid4861 <45942534+sid4861@users.noreply.github.com>
2020-12-15 02:07:35 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var txtInput = document.querySelector('#text-input');
+var txtOutput = document.querySelector('#text-output');
+var button = document.querySelector('#button');
+
+var url = 'https://api.funtranslations.com/translate/minion.json?text=';
+
+function getUrl(text) {
+    return url + text;
+
+}
+
+button.addEventListener('click', function () {
+    fetch(getUrl(txtInput.value))
+        .then(response => response.json())
+        .then(json => txtOutput.innerText = json.contents.translated)
+        .catch(err => console.log(err));
+});
---
+++ index.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./app.css" />
+</head>
+
+<body>
+
+    <header>
+        <div>
+            <h1>Banana Conversations</h1>
+        </div>
+    </header>
+
+    <textarea placeholder="Enter text to convert to banana language" id="text-input">
+
+    </textarea>
+
+
+    <button id="button">Translate</button>
+    <p style="text-align: center;">translation will come here 🖖</p>
+    <div id="text-output">
+    </div>
+    <footer>
+        <div>
+            <h1>about</h1>
+            <p style="max-width: 600px;margin: auto">
+                Are you a fan of minions? Did you know that the gibberish they say is an actual language. Use the
+                translator to convert your text from English to Minion speak or Banana language.
+            </p>
+        </div>
+    </footer>
+    <script src="app.js" type="text/javascript"></script>
+</body>
+
+</html>
